1792434683f3e558f15af5fdb1d40cc4a8a1df25
Lucas Adriano Dias <61661218+adriano328@users.noreply.github.com>
2024-06-15 09:34:26 -0400

    Add files via upload

---
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -0,0 +1,42 @@
+package me.dio.academia.digital.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "aluno")
+@JsonIgnoreProperties({"hibernateLazyInitializar","handler"})
+public class Aluno {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(name = "nome")
+  private String nome;
+
+  @Column(name = "cpf", unique = true)
+  private String cpf;
+
+  @Column(name = "bairro")
+  private String bairro;
+
+  @Column(name = "data_nascimento")
+  private LocalDate dataDeNascimento;
+
+  @OneToMany(mappedBy = "aluno", fetch = FetchType.LAZY)
+  @JsonIgnore
+  private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
+
+}
---
+++ src/main/java/me/dio/academia/digital/service/AvaliacaoFisicaImpl.java
@@ -0,0 +1,54 @@
+package me.dio.academia.digital.service;
+
+import me.dio.academia.digital.entity.Aluno;
+import me.dio.academia.digital.entity.AvaliacaoFisica;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaForm;
+import me.dio.academia.digital.entity.form.AvaliacaoFisicaUpdateForm;
+import me.dio.academia.digital.repository.AlunoRepository;
+import me.dio.academia.digital.repository.AvaliacaoFisicaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AvaliacaoFisicaImpl implements IAvaliacaoFisicaService{
+
+    @Autowired
+    private AvaliacaoFisicaRepository avaliacaoFisicaRepository;
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    @Override
+    public AvaliacaoFisica create(AvaliacaoFisicaForm form) {
+
+       Aluno aluno = alunoRepository.findById(form.getAlunoId()).get();
+        AvaliacaoFisica avaliacaoFisica = new AvaliacaoFisica();
+        avaliacaoFisica.setPeso(form.getPeso());
+        avaliacaoFisica.setAltura(form.getAltura());
+        avaliacaoFisica.setAluno(aluno);
+
+        return avaliacaoFisicaRepository.save(avaliacaoFisica);
+    }
+
+    @Override
+    public AvaliacaoFisica get(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<AvaliacaoFisica> getAll() {
+        return null;
+    }
+
+    @Override
+    public AvaliacaoFisica update(Long id, AvaliacaoFisicaUpdateForm formUpdate) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
